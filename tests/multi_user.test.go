0c44b8e2ac12a7da58056a607b858f0d65095627
Guruprasad BR <brguru90@gmail.com>
2022-06-15 16:34:25 +0530

    .

--- tests/multi_user.test.go
+++ tests/multi_user.test.go
@@ -12,7 +12,7 @@ func TestAsMultiUser() {
 	fmt.Println("--> LoginAsMultiUser finished")
 	my_modules.LogToJSON(user.GetUserDetailAsMultiUser("http://localhost:8000/api/user/", 100000, 10000), nil)
 	go func() {
-		my_modules.LogToJSON(api_requests.SignUp(20000, 100), "./log2.json")
+		my_modules.LogToJSON(api_requests.SignUp(400, 100), "./log2.json")
 	}()
 	my_modules.LogToJSON(user.GetUserDetailAsMultiUser("http://localhost:8000/api/user/?page=1000&limit=20", 100000, 10000), nil)
 }
