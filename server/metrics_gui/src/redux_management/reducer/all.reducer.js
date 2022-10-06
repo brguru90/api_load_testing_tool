0905fd4a87bd03e8098f2aec526864c796b9358d
Guruprasad BR <brguru90@gmail.com>
2022-10-06 19:15:08 +0530

    ui in progress

--- server/metrics_gui/src/redux_management/reducer/all.reducer.js
+++ server/metrics_gui/src/redux_management/reducer/all.reducer.js
@@ -3,7 +3,7 @@ import reducer_2 from "./reducer2"
 import {combineReducers} from "redux"
 
 const allReducer = combineReducers({
-    metric_data: metric_reducer,
+    metrics_data: metric_reducer,
     data_2: reducer_2,
 })
 export default allReducer
