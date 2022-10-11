6f086e77cde33a728c1cb7e1f2a24a46dba421f7
Guruprasad BR <brguru90@gmail.com>
2022-10-11 17:26:29 +0530

    .

--- benchmark/util.go
+++ benchmark/util.go
@@ -17,14 +17,14 @@ func RunBenchmark(callback func()) {
 		go func() {
 			http.ListenAndServe("localhost:7777", nil)
 			// http://localhost:7777/debug/pprof/
-			// go tool pprof --http=localhost:8800 /home/justdial/Desktop/test_workspace/api_load_testing_tool/__debug_bin ./profile
+			// go tool pprof --http=localhost:8800 /home/guruprasad/Desktop/test_workspace/api_load_testing_tool/__debug_bin ./profile
 		}()
 	}
 
 	go func() {
 		my_modules.InitBeforeBenchMarkStart()
 		if gin_mode != "release" {
-			// go tool pprof --http=localhost:8800 /home/justdial/Desktop/test_workspace/api_load_testing_tool/__debug_bin /home/justdial/Desktop/test_workspace/api_load_testing_tool/mem.pprof
+			// go tool pprof --http=localhost:8800 /home/guruprasad/Desktop/test_workspace/api_load_testing_tool/__debug_bin /home/guruprasad/Desktop/test_workspace/api_load_testing_tool/mem.pprof
 
 			memprof, err := os.Create("mem.pprof")
 			callback()
