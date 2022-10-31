0a364e739c08e804c0c9402f8cd0a2620a731b24
Guruprasad BR <brguru90@gmail.com>
2022-10-31 13:23:18 +0530

    remove delay in processing data

--- benchmark/my_modules/api_benchmark_multiuser.util.go
+++ benchmark/my_modules/api_benchmark_multiuser.util.go
@@ -72,9 +72,8 @@ func BenchmarkAPIAsMultiUser(
 	}
 
 	var concurrent_req_wg, rh_concurrent_req_wg sync.WaitGroup
-	var iteration_wg, rh_iteration_wg sync.WaitGroup
-
-	var i, j, total_time_to_complete_api, avg_time_to_complete_api, avg_time_to_connect_api, avg_time_to_receive_first_byte_api int64
+	var all_concurrent_iteration_wg,iteration_wg2, rh_iteration_wg sync.WaitGroup
+	var i, j int64
 
 	requests_ahead := make(chan CreatedAPIRequestFormat, number_of_iteration*concurrent_request)
 
@@ -111,203 +110,217 @@ func BenchmarkAPIAsMultiUser(
 	requests_ahead = nil
 
 	iterations_start_time := time.Now()
+	all_concurrent_iteration_wg.Add(int(number_of_iteration))
 	for i = 0; i < number_of_iteration; i++ {
-		messages := make(chan MessageType)
-		additional_details := make(chan AdditionalAPIDetails, concurrent_request)
-		fmt.Printf("url=%s,i=%v\n", _url, i)
+		iteration_wg2.Add(1)
+		go func(_i int64) {
+			defer all_concurrent_iteration_wg.Done()
+			var iteration_wg sync.WaitGroup
+			iteration_wg.Add(1)
+			messages := make(chan MessageType)
+			additional_details := make(chan AdditionalAPIDetails, concurrent_request)
+			fmt.Printf("url=%s,i=%v\n", _url, _i)
 
-		concurrent_req_start_time := time.Now()
-		// run whole parallel request routine in background
-		// so that i can loop through channel data later
-		iteration_wg.Add(1)
-		go func(main_iteration int64) {
-			defer iteration_wg.Done()
-			// spin up all request parallally & wait for all those to finish
-			concurrent_req_wg.Add(int(concurrent_request))
-			// todo:
-			// here benchmark server will distribute request to runner client
-			// wait for all to complete
-			// & then collect metrics
-			for j = 0; j < concurrent_request; j++ {
-				// fmt.Printf("%v-%v\n", i, j)
-				go func(sub_iteration int64) {
-					defer func() {
-						request_ahead_array[sub_iteration] = CreatedAPIRequestFormat{
-							req:          nil,
-							err:          nil,
-							payload:      nil,
-							request_size: 0,
+			concurrent_req_start_time := time.Now()
+			// run whole parallel request routine in background
+			// so that i can loop through channel data later
+			go func(main_iteration int64) {
+				// spin up all request parallally & wait for all those to finish
+				concurrent_req_wg.Add(int(concurrent_request))
+				// todo:
+				// here benchmark server will distribute request to runner client
+				// wait for all to complete
+				// & then collect metrics
+				for j = 0; j < concurrent_request; j++ {
+					// fmt.Printf("%v-%v\n", i, j)
+					go func(sub_iteration int64) {
+						defer func() {
+							request_ahead_array[sub_iteration] = CreatedAPIRequestFormat{
+								req:          nil,
+								err:          nil,
+								payload:      nil,
+								request_size: 0,
+							}
+							concurrent_req_wg.Done()
+						}()
+
+						data, time_to_complete_api, _, err := APIReq(&request_ahead_array[sub_iteration], response_interceptor, additional_details)
+						// fmt.Printf("finish APIReq\n")
+						messages <- MessageType{
+							Data:                 data,
+							Time_to_complete_api: time_to_complete_api,
+							Err:                  err,
 						}
-						concurrent_req_wg.Done()
-					}()
+						// fmt.Printf("finish channel\n")
+					}((main_iteration * concurrent_request) + j)
+				}
+				concurrent_req_wg.Wait()
+				// fmt.Println("all the parallel request finished")
+				close(messages)
+				close(additional_details)
+				iteration_wg.Done()
+				iteration_wg2.Done()
+			}(_i)
 
-					data, time_to_complete_api, _, err := APIReq(&request_ahead_array[sub_iteration], response_interceptor, additional_details)
-					// fmt.Printf("finish APIReq\n")
-					messages <- MessageType{
-						Data:                 data,
-						Time_to_complete_api: time_to_complete_api,
-						Err:                  err,
-					}
-					// fmt.Printf("finish channel\n")
-				}((main_iteration * concurrent_request) + j)
+			var avg_time_to_complete_api, avg_time_to_connect_api, avg_time_to_receive_first_byte_api int64
+
+
+			avg_time_to_complete_api = 0
+			avg_time_to_connect_api = 0
+			avg_time_to_receive_first_byte_api = 0
+			min_time_to_complete_api := math.Inf(1)
+			max_time_to_complete_api := 0.0
+			status_codes := make(map[int]int64)
+			// looping through channel data, whenever go routine finishes execution
+			// fmt.Println("loop through msgs")
+			for message := range messages {
+				avg_time_to_complete_api += message.Time_to_complete_api
+				avg_time_to_connect_api += message.Data.context_data.time_to_connect
+				avg_time_to_receive_first_byte_api += message.Data.context_data.ttfb
+				var cur_status_code int = message.Data.context_data.status_code
+				status_codes[cur_status_code] += 1
+
+				if float64(message.Time_to_complete_api) < min_time_to_complete_api {
+					min_time_to_complete_api = float64(message.Time_to_complete_api)
+				}
+
+				if float64(message.Time_to_complete_api) > max_time_to_complete_api {
+					max_time_to_complete_api = float64(message.Time_to_complete_api)
+				}
 			}
-			concurrent_req_wg.Wait()
-			// fmt.Println("all the parallel request finished")
-			close(messages)
-			close(additional_details)
+			// fmt.Println("finish loop through msgs")
+			// fmt.Println("finish wait")
+			iteration_wg.Wait()
+			concurrent_req_end_time := time.Now()
+
+			avg_time_to_complete_api = avg_time_to_complete_api / concurrent_request
+			avg_time_to_connect_api = avg_time_to_connect_api / concurrent_request
+			avg_time_to_receive_first_byte_api = avg_time_to_receive_first_byte_api / concurrent_request
+
+			status_code_in_percentage := make(map[int]float64)
+			for status_code, occurrence := range status_codes {
+				status_code_in_percentage[status_code] = (float64(occurrence) / float64(concurrent_request)) * 100
+			}
+
+			var avg_request_payload_size float64 = 0
+			var avg_response_payload_size float64 = 0
+			var additional_details_arr []AdditionalAPIDetails
+			for additional_detail := range additional_details {
+				additional_details_arr = append(additional_details_arr, additional_detail)
+				avg_request_payload_size += float64(additional_detail.request_payload_size)
+				avg_response_payload_size += float64(additional_detail.response_payload_size)
+			}
+			if len(additional_details_arr) > 0 {
+				avg_request_payload_size = avg_request_payload_size / float64(len(additional_details_arr))
+				avg_response_payload_size = avg_response_payload_size / float64(len(additional_details_arr))
+			}
+
+			// concurrent_req_start_time & end time will not be perfectly accurate
+			// better to take
+			// min(additional_detail.request_sent)
+			// max(additional_detail.request_processed)
+			track_iteration_start_time := concurrent_req_start_time
+			track_iteration_end_time := concurrent_req_end_time
+			if len(additional_details_arr) > 0 {
+
+				track_iteration_start_time := additional_details_arr[0].request_sent
+				track_iteration_end_time := additional_details_arr[0].request_processed
+				for _, val := range additional_details_arr {
+					if val.request_sent.Before(track_iteration_start_time) {
+						track_iteration_start_time = val.request_sent
+					}
+					if val.request_processed.After(track_iteration_end_time) {
+						track_iteration_end_time = val.request_processed
+					}
+				}
+			}
+			track_iteration_time := track_iteration_start_time
+			prev_iteration_time := track_iteration_start_time.Add(time.Nanosecond * -1)
+			if track_iteration_time.Add(time.Second * 1).After(track_iteration_end_time) {
+				track_iteration_time = track_iteration_end_time
+			} else {
+				track_iteration_time = track_iteration_time.Add(time.Second * 1)
+			}
+			var per_second_metrics []BenchMarkPerSecondCount
+			// var _request_sent_in_sec_avg, _request_connected_in_sec_avg, _request_processed_in_sec_avg int64
+			if len(additional_details_arr) > 0 {
+				for {
+					// fmt.Printf("prev_iteration_time=%v,track_iteration_time=%v\n",prev_iteration_time,track_iteration_time)
+					var _request_sent_in_sec, _request_connected_in_sec, _request_received_first_byte_in_sec, _request_processed_in_sec int64
+					var request_payload_size float64 = 0
+					var response_payload_size float64 = 0
+					for _, additional_detail := range additional_details_arr {
+
+						if additional_detail.request_sent.After(prev_iteration_time) && (additional_detail.request_sent.Equal(track_iteration_time) || additional_detail.request_sent.Before(track_iteration_time)) {
+							_request_sent_in_sec++
+							request_payload_size += float64(additional_detail.request_payload_size)
+						}
+						if additional_detail.request_connected.After(prev_iteration_time) && (additional_detail.request_connected.Equal(track_iteration_time) || additional_detail.request_connected.Before(track_iteration_time)) {
+							_request_connected_in_sec++
+						}
+						if additional_detail.request_receives_first_byte.After(prev_iteration_time) && (additional_detail.request_receives_first_byte.Equal(track_iteration_time) || additional_detail.request_receives_first_byte.Before(track_iteration_time)) {
+							_request_received_first_byte_in_sec++
+						}
+						if additional_detail.request_processed.After(prev_iteration_time) && (additional_detail.request_processed.Equal(track_iteration_time) || additional_detail.request_processed.Before(track_iteration_time)) {
+							_request_processed_in_sec++
+							response_payload_size += float64(additional_detail.response_payload_size)
+						}
+					}
+					per_second_metrics = append(per_second_metrics, BenchMarkPerSecondCount{
+						From_time_duration:                   prev_iteration_time,
+						To_time_duration:                     track_iteration_time,
+						Request_sent:                         _request_sent_in_sec,
+						Request_connected:                    _request_connected_in_sec,
+						Request_processed:                    _request_processed_in_sec,
+						Request_receives_first_byte:          _request_received_first_byte_in_sec,
+						Total_request_payload_size_in_bytes:  request_payload_size,
+						Total_response_payload_size_in_bytes: response_payload_size,
+					})
+
+					// iterate over elapsed time
+					if track_iteration_time.After(track_iteration_end_time) || track_iteration_time.Equal(track_iteration_end_time) {
+						break
+					}
+					prev_iteration_time = track_iteration_time
+					if track_iteration_time.Add(time.Second * 1).After(track_iteration_end_time) {
+						track_iteration_time = track_iteration_end_time
+					} else {
+						track_iteration_time = track_iteration_time.Add(time.Second * 1)
+					}
+				}
+			}
+
+			temp_data := BenchmarkData{
+				Url:                             _url,
+				Status_code_in_percentage:       status_code_in_percentage,
+				Status_codes:                    status_codes,
+				Concurrent_request:              concurrent_request,
+				Avg_time_to_connect_api:         avg_time_to_connect_api,
+				Avg_time_to_receive_first_byte:  avg_time_to_receive_first_byte_api,
+				Avg_time_to_complete_api:        avg_time_to_complete_api,
+				Min_time_to_complete_api:        int64(min_time_to_complete_api),
+				Max_time_to_complete_api:        int64(max_time_to_complete_api),
+				Average_request_payload_size:    avg_request_payload_size,
+				Average_response_payload_size:   avg_response_payload_size,
+				Total_time_to_complete_all_apis: concurrent_req_end_time.Sub(concurrent_req_start_time).Milliseconds(),
+				Benchmark_per_second_metric:     per_second_metrics,
+				IterationID:                     _i,
+			}
+			result := BenchmarkMetricStruct{
+				Url:           temp_data.Url,
+				IterationData: []BenchmarkData{temp_data},
+				ProcessUid:    process_uuid,
+			}
+			pushBenchMarkMetrics(result)
+			each_iterations_data = append(each_iterations_data, temp_data)
+
 		}(i)
-
-		avg_time_to_complete_api = 0
-		avg_time_to_connect_api = 0
-		avg_time_to_receive_first_byte_api = 0
-		min_time_to_complete_api := math.Inf(1)
-		max_time_to_complete_api := 0.0
-		status_codes := make(map[int]int64)
-		// looping through channel data, whenever go routine finishes execution
-		// fmt.Println("loop through msgs")
-		for message := range messages {
-			avg_time_to_complete_api += message.Time_to_complete_api
-			avg_time_to_connect_api += message.Data.context_data.time_to_connect
-			avg_time_to_receive_first_byte_api += message.Data.context_data.ttfb
-			var cur_status_code int = message.Data.context_data.status_code
-			status_codes[cur_status_code] += 1
-
-			if float64(message.Time_to_complete_api) < min_time_to_complete_api {
-				min_time_to_complete_api = float64(message.Time_to_complete_api)
-			}
-
-			if float64(message.Time_to_complete_api) > max_time_to_complete_api {
-				max_time_to_complete_api = float64(message.Time_to_complete_api)
-			}
-		}
-		// fmt.Println("finish loop through msgs")
-		iteration_wg.Wait() // just wait here before doing next iteration
-		// fmt.Println("finish wait")
-		concurrent_req_end_time := time.Now()
-
-		avg_time_to_complete_api = avg_time_to_complete_api / concurrent_request
-		avg_time_to_connect_api = avg_time_to_connect_api / concurrent_request
-		avg_time_to_receive_first_byte_api = avg_time_to_receive_first_byte_api / concurrent_request
-
-		status_code_in_percentage := make(map[int]float64)
-		for status_code, occurrence := range status_codes {
-			status_code_in_percentage[status_code] = (float64(occurrence) / float64(concurrent_request)) * 100
-		}
-
-		var avg_request_payload_size float64 = 0
-		var avg_response_payload_size float64 = 0
-		var additional_details_arr []AdditionalAPIDetails
-		for additional_detail := range additional_details {
-			additional_details_arr = append(additional_details_arr, additional_detail)
-			avg_request_payload_size += float64(additional_detail.request_payload_size)
-			avg_response_payload_size += float64(additional_detail.response_payload_size)
-		}
-		if len(additional_details_arr) > 0 {
-			avg_request_payload_size = avg_request_payload_size / float64(len(additional_details_arr))
-			avg_response_payload_size = avg_response_payload_size / float64(len(additional_details_arr))
-		}
-
-		// concurrent_req_start_time & end time will not be perfectly accurate
-		// better to take
-		// min(additional_detail.request_sent)
-		// max(additional_detail.request_processed)
-		track_iteration_start_time := concurrent_req_start_time
-		track_iteration_end_time := concurrent_req_end_time
-		if len(additional_details_arr)>0 {
-
-			track_iteration_start_time := additional_details_arr[0].request_sent
-			track_iteration_end_time := additional_details_arr[0].request_processed
-			for _, val := range additional_details_arr {
-				if val.request_sent.Before(track_iteration_start_time) {
-					track_iteration_start_time = val.request_sent
-				}
-				if val.request_processed.After(track_iteration_end_time) {
-					track_iteration_end_time = val.request_processed
-				}
-			}
-		}
-		track_iteration_time := track_iteration_start_time
-		prev_iteration_time := track_iteration_start_time.Add(time.Nanosecond * -1)
-		if track_iteration_time.Add(time.Second * 1).After(track_iteration_end_time) {
-			track_iteration_time = track_iteration_end_time
-		} else {
-			track_iteration_time = track_iteration_time.Add(time.Second * 1)
-		}
-		var per_second_metrics []BenchMarkPerSecondCount
-		// var _request_sent_in_sec_avg, _request_connected_in_sec_avg, _request_processed_in_sec_avg int64
-		if len(additional_details_arr) > 0 {
-			for {
-				// fmt.Printf("prev_iteration_time=%v,track_iteration_time=%v\n",prev_iteration_time,track_iteration_time)
-				var _request_sent_in_sec, _request_connected_in_sec, _request_received_first_byte_in_sec, _request_processed_in_sec int64
-				var request_payload_size float64 = 0
-				var response_payload_size float64 = 0
-				for _, additional_detail := range additional_details_arr {
-
-					if additional_detail.request_sent.After(prev_iteration_time) && (additional_detail.request_sent.Equal(track_iteration_time) || additional_detail.request_sent.Before(track_iteration_time)) {
-						_request_sent_in_sec++
-						request_payload_size += float64(additional_detail.request_payload_size)
-					}
-					if additional_detail.request_connected.After(prev_iteration_time) && (additional_detail.request_connected.Equal(track_iteration_time) || additional_detail.request_connected.Before(track_iteration_time)) {
-						_request_connected_in_sec++
-					}
-					if additional_detail.request_receives_first_byte.After(prev_iteration_time) && (additional_detail.request_receives_first_byte.Equal(track_iteration_time) || additional_detail.request_receives_first_byte.Before(track_iteration_time)) {
-						_request_received_first_byte_in_sec++
-					}
-					if additional_detail.request_processed.After(prev_iteration_time) && (additional_detail.request_processed.Equal(track_iteration_time) || additional_detail.request_processed.Before(track_iteration_time)) {
-						_request_processed_in_sec++
-						response_payload_size += float64(additional_detail.response_payload_size)
-					}
-				}
-				per_second_metrics = append(per_second_metrics, BenchMarkPerSecondCount{
-					From_time_duration:                   prev_iteration_time,
-					To_time_duration:                     track_iteration_time,
-					Request_sent:                         _request_sent_in_sec,
-					Request_connected:                    _request_connected_in_sec,
-					Request_processed:                    _request_processed_in_sec,
-					Request_receives_first_byte:          _request_received_first_byte_in_sec,
-					Total_request_payload_size_in_bytes:  request_payload_size,
-					Total_response_payload_size_in_bytes: response_payload_size,
-				})
-
-				// iterate over elapsed time
-				if track_iteration_time.After(track_iteration_end_time) || track_iteration_time.Equal(track_iteration_end_time) {
-					break
-				}
-				prev_iteration_time = track_iteration_time
-				if track_iteration_time.Add(time.Second * 1).After(track_iteration_end_time) {
-					track_iteration_time = track_iteration_end_time
-				} else {
-					track_iteration_time = track_iteration_time.Add(time.Second * 1)
-				}
-			}
-		}
-
-		temp_data := BenchmarkData{
-			Url:                             _url,
-			Status_code_in_percentage:       status_code_in_percentage,
-			Status_codes:                    status_codes,
-			Concurrent_request:              concurrent_request,
-			Avg_time_to_connect_api:         avg_time_to_connect_api,
-			Avg_time_to_receive_first_byte:  avg_time_to_receive_first_byte_api,
-			Avg_time_to_complete_api:        avg_time_to_complete_api,
-			Min_time_to_complete_api:        int64(min_time_to_complete_api),
-			Max_time_to_complete_api:        int64(max_time_to_complete_api),
-			Average_request_payload_size:    avg_request_payload_size,
-			Average_response_payload_size:   avg_response_payload_size,
-			Total_time_to_complete_all_apis: concurrent_req_end_time.Sub(concurrent_req_start_time).Milliseconds(),
-			Benchmark_per_second_metric:     per_second_metrics,
-			IterationID:                     i,
-		}
-		result := BenchmarkMetricStruct{
-			Url:           temp_data.Url,
-			IterationData: []BenchmarkData{temp_data},
-			ProcessUid:    process_uuid,
-		}
-		pushBenchMarkMetrics(result)
-		each_iterations_data = append(each_iterations_data, temp_data)
-
+		iteration_wg2.Wait()
 	}
+	all_concurrent_iteration_wg.Wait()
 	iterations_end_time := time.Now()
+
+	var total_time_to_complete_api,avg_time_to_complete_api,avg_time_to_connect_api int64
 
 	avg_time_to_complete_api = 0
 	avg_time_to_connect_api = 0
